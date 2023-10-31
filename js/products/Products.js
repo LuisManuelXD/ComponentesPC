b1250d79967a6fd2b673b467f10d881973e29f08
Luis Manuel Rios <riostorresGamer@gmail.com>
2023-10-31 08:18:54 -0600

    PC-1 se implementa la busqueda de productos

--- js/products/Products.js
+++ js/products/Products.js
@@ -1,25 +1,30 @@
+let search = '';
+let products;
+let inputSearch = document.querySelector("#txtSearch");
+
 getProducts();
-function getProducts() {
+
+function getProducts(searchTerm = '') {
     let cards = document.querySelector("#cards");
 
     let ajax = new XMLHttpRequest();
         ajax.open( 'get', '/php/product/GetAll.php', true );
         ajax.onload = function() {
-            let products = JSON.parse( ajax.response );
-            console.log(products);
+            products = JSON.parse( ajax.response );
             let templateCards = '';
 
             products.forEach(product => {
+              if(product.name.toLowerCase().includes(searchTerm.toLowerCase())) {
                 if(parseInt(product.available) > 0) {
                     templateCards += `<div id="${product.id}" class="product">
                     <div class="product-image">
-                      <a title="${product.name}" href="#">
+                      <a title="${product.name}" href="/pages/products/item/${product.id}">
                         <img src="/assets/img/llamaTactica.png" alt="" />
                       </a>
                     </div>
                     <div class="product-info">
                       <h4>
-                        <a href="#">${product.name}</a>
+                        <a href="/pages/products/item/${product.id}">${product.name}</a>
                       </h4>
                       <div class="product-price">
                         <span class="price">$${product.price}</span>
@@ -35,13 +40,13 @@ function getProducts() {
                 } else {
                     templateCards += `<div id="${product.id}" class="product">
                     <div class="product-image">
-                      <a title="${product.name}" href="#">
+                      <a title="${product.name}" href="/pages/products/item/${product.id}">
                         <img src="/assets/img/llamaTactica.png" alt="" />
                       </a>
                     </div>
                     <div class="product-info">
                       <h4>
-                        <a href="#">${product.name}</a>
+                        <a href="/pages/products/item/${product.id}">${product.name}</a>
                       </h4>
                       <div class="product-price">
                         <span class="price">$${product.price}</span>
@@ -55,8 +60,22 @@ function getProducts() {
                     </div>
                   </div>`;
                 }
+              } else {
+                templateCards = `<div class="card-search">
+                <img
+                  src="/assets/svg/illustrations/oc-lost.svg"
+                  alt="Image Description"
+                />
+                <h3>Por el momento no tenemos resultados para esta búsqueda.</h3>
+              </div>`;
+              }
             });
             cards.innerHTML = templateCards;
         }
         ajax.send();
 }
+
+inputSearch.addEventListener('keyup', (e) => {
+  search = e.target.value;
+  getProducts(search);
+});
