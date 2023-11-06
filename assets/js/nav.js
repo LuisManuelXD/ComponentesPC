d8cb42be91ef35fe9c337cb4213eaab6afa2dd0b
Luis Manuel Rios <riostorresGamer@gmail.com>
2023-11-06 01:20:34 -0600

    PC-1 Agregando responsivo en pantallas mas pequenas en admin

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,4 +1,6 @@
 let listElements = document.querySelectorAll(".list__button--click");
+let listMovil = document.querySelectorAll('.nav-admin')[0];
+let expandido = false;
 
 listElements.forEach((listElement) => {
   listElement.addEventListener("click", () => {
@@ -12,4 +14,16 @@ listElements.forEach((listElement) => {
 
     menu.style.height = `${height}px`;
   });
+});
+
+listMovil.addEventListener("click", (event) => {
+  if(window.innerWidth <= 920) {
+    if(!expandido && (event.target.classList.contains('list') || event.target.classList.contains('nav-admin'))) {
+      listMovil.style.width = "2em";
+      expandido = true;
+    } else if (!event.target.classList.contains('list') && !event.target.classList.contains('nav-admin')) {
+      listMovil.style.width = "15em";
+      expandido = false;
+    }
+  }
 });
