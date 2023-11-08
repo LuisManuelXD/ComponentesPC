fe4ca854b44dc0b8f5c6e44ab17527e3bdf0be98
Luis Manuel <riostorresGamer@gmail.com>
2023-11-08 00:38:00 -0600

    PC-1 Peticiones a mysql para admin productos

---
+++ php/admin/product/Delete.php
@@ -0,0 +1,13 @@
+<?php
+include "../../conexion.php";
+
+$id = $_POST['id'];
+
+$em = "delete from product where id='$id';";
+$query = mysqli_query($conexion, $em);
+
+if(!$query)
+    die ("Fallo la eliminacion." . mysqli_error($conexion));
+
+echo 'Producto eliminado correctamente.';
+?>
