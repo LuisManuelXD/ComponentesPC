2a97f1c597a07c1e03e24936c596e3c08114f93e
Luis Manuel Rios <riostorresGamer@gmail.com>
2023-12-05 19:52:56 -0600

    PC-1 Libreria no me funciono

--- js/pdf.js
+++
@@ -1,7 +0,0 @@
-function crear() {
-    // Default export is a4 paper, portrait, using millimeters for units
-    const doc = new jsPDF();
-    
-    doc.text("Hello world!", 10, 50);
-    doc.save("a4.pdf");
-}
